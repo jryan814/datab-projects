e7e50082994478bda71c39eb4331eb74b57f643a
Jonathan Hanna <jonathanhanna814@gmail.com>
2021-08-31 12:35:15 -0600

    Added multiprocessing and improved documentation

--- gov_contracts_pipeline/gov_contract_data_cleaner.py
+++ gov_contracts_pipeline/gov_contract_data_cleaner.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 3.8.3
 # coding: utf-8
 
 # # Government Contracts Data Cleaning
@@ -6,44 +6,82 @@
 # Should work on most downloaded datasets with similar criteria (filtered by NAICS code, and small business).
 
 # imports
+from multiprocessing import freeze_support, Pool
 import pandas as pd
 import os
-# import numpy as np
-# import datetime as dt
+from time import time
 
+
+# import various settings
 from settings import DEF_DATA_FILE, DATA_PATH, rename_map, DEF_CLEAN_PATH
 
+start = time()
 
+def pipeline(df: pd.DataFrame, *args, **kwargs) -> pd.DataFrame:
+    """Initial transformations
 
-def pipeline(df, *args, **kwargs):
-    keep_list = [k for k in rename_map]
-    df = df[keep_list]
+    Args:
+        df (pd.DataFrame): Raw dataset
+
+    Returns:
+        pd.DataFrame: Initial processed data
+    """    
+    # No longer needed
+    # keep_list = [k for k in rename_map]
+    # df = df[keep_list] 
+    
     df = df.rename(columns=rename_map)
     df = df.drop(columns=['city_local_government',
                                 'award_or_idv_flag',
                                 'parent_award_mod_number'])
     df = df[df.columns.sort_values()]
+
+    # Removes any actions with 0 dollars_obligated
     df = df[df['dollars_obligated'] != 0].copy()
     return df
 
-def fill_nulls(df):
+def fill_nulls(df: pd.DataFrame) -> pd.DataFrame:
+    """## Clunky NaN resolution (names, ids, and descriptions)
+    imputes nan values with data from other columns
+
+    Args:
+        df (pd.DataFrame): The data
+
+    Returns:
+        pd.DataFrame: Resolved data
+    """    
     df['parent_award_agency_name'] = df['parent_award_agency_name'].fillna(df['awarding_agency_name'])
     df['awarding_agency_name'] = df['awarding_agency_name'].fillna(df['parent_award_agency_name'])
     df['parent_award_agency_id'] = df['parent_award_agency_id'].fillna(df['awarding_sub_agency_code'])
     df['award_description'] = df['award_description'].fillna('unknown')
     df['award_id'] = df['award_id'].fillna(df['award_piid_ref'])
-    df['recipient_duns'] =  df['recipient_duns'].astype('object')
     return df
 
 # resolve missing data for international places of performance
-def fix_international(df):
+def fix_international(df: pd.DataFrame) -> pd.DataFrame:
+    """## Fills missing locale info due to internationality
+
+    Args:
+        df (pd.DataFrame): The data
+
+    Returns:
+        pd.DataFrame: Data with fixed locale data
+    """    
     for col in df.columns:
         if 'plop' in col:
             df[col] = df[col].fillna(df['plop_country_code'])
     return df
 
 # Resolve minor name variations
-def reformat_names(name):
+def reformat_names(name: str) -> str:
+    """## Resolves most naming inconsistencies
+
+    Args:
+        name (str): name string
+
+    Returns:
+        str: Resolved name string
+    """    
     if pd.isnull(name):
         return 'missing'
     name = name.replace(
@@ -54,7 +92,16 @@ def reformat_names(name):
     return name
 
 # Impute missing/err values
-def fix_date_issues(date_):
+def fix_date_issues(date_: str) -> str:
+    """## Fixes the date formatting issue/inconsistency
+    pandas to_datetime() was unable to resolve some of the date formats
+
+    Args:
+        date_ (str): Date string
+
+    Returns:
+        str: Reformatted date string
+    """    
     if pd.isnull(date_):
         return
     date_ = str(date_)
@@ -63,7 +110,15 @@ def fix_date_issues(date_):
     return date_
 
 # convert date columns to datetime dtype
-def dtype_conversions(df):
+def dtype_conversions(df: pd.DataFrame) -> pd.DataFrame:
+    """## Converts date columns to datetime
+
+    Args:
+        df (pd.DataFrame): target data
+
+    Returns:
+        pd.DataFrame: trsnsformed data
+    """    
     for col in df.columns:
         if 'fiscal_year' in col:
             continue
@@ -76,7 +131,15 @@ def dtype_conversions(df):
     return df
 
 # add 5 digit zip code
-def add_zip_str(df):
+def add_zip_str(df: pd.DataFrame) -> pd.DataFrame:
+    """## Creates new column with formatted zip
+
+    Args:
+        df (pd.DataFrame): Raw data
+
+    Returns:
+        pd.DataFrame: Transformed data
+    """    
     cols = df.columns
     for col in cols:
         if 'zip' in col and '_5' not in col:
@@ -85,8 +148,16 @@ def add_zip_str(df):
     return df
 
 # Runs the rest of the pipeline
-def run_pipeline2(df):
-    
+def run_pipeline2(df: pd.DataFrame) -> pd.DataFrame:
+    """## Runs entire pipeline
+
+    Args:
+        df (pd.DataFrame): The data to transform.
+
+    Returns:
+        pd.DataFrame: Transformed data
+    """    
+    df = pipeline(df)
     df = fill_nulls(df)
     df = fix_international(df)
     df.loc[:,'recipient_name'] = df.loc[:,'recipient_name'].apply(reformat_names)
@@ -104,7 +175,7 @@ def run_pipeline2(df):
     df = add_zip_str(df)
     return df
 
-def make_csv(data, fname=None, update=True):
+def make_csv(data: pd.DataFrame, fname: str=None, update: bool=True):
     if update:
         if not fname:
             fname = DEF_CLEAN_PATH
@@ -112,15 +183,16 @@ def make_csv(data, fname=None, update=True):
             fname = os.path.join(DATA_PATH, fname)
         data.to_csv(fname, index=False)
 
-def exec_pipeline(fn=None):
-    '''
-    Executes the entire cleaning of the data.
-    Creates new csv_file of the cleaned data (named 'cleaned_data.csv' in the data directory)
-    args:
-        fn: (str, or path-like) file name for dirty data
-    returns:
-        cleaned data
-    '''
+
+def exec_pipeline(fn: str=None) -> pd.DataFrame:
+    """## Execute cleaning pipeline (slow non-parallel version)
+
+    Args:
+        fn (str, optional): Filename of dirty data. Defaults to None.
+
+    Returns:
+        pd.DataFrame: Cleaned data
+    """    
     if fn:
         if '/' in fn or '\\' in fn:
             file_name = fn
@@ -128,17 +200,42 @@ def exec_pipeline(fn=None):
             file_name = os.path.join(DATA_PATH, fn)
     else:
         file_name = DEF_DATA_FILE
-    data = pd.read_csv(file_name, low_memory=False)
-    data = pipeline(data)
+    data = pd.read_csv(file_name, usecols=[c for c in rename_map], low_memory=False)
+    # data = pipeline(data)
     data = run_pipeline2(data)
     data = data.fillna('unknown')
     make_csv(data)
     return data
 
-       
-    
+def exec_pool(fn: str=None, num_processes: int=5) -> pd.DataFrame:
+    """## Executes the Data Cleaning pipeline using multiprocessing pools
 
-    
+    Args:
+        fn (str, optional): Filename. Defaults to None.
+        num_processes (int, optional): Number of processes and chunks. Defaults to 5.
 
+    Returns:
+        pd.DataFrame: Cleaned dataframe
+    """
+    if not fn:
+        fn = os.path.join(DATA_PATH, 'USAspending_award_summaries.csv')
+    else:
+        fn = os.path.join(DATA_PATH, fn)
+
+    chunks = pd.read_csv(fn, chunksize=100000, usecols=[c for c in rename_map], low_memory=False)
     
+    with Pool(num_processes) as pool:
+        chunk_results = pool.map(run_pipeline2, chunks)
+  
+    data = pd.concat(chunk_results)
+    data = data.fillna('unknown')
+
+    return data
+
+if __name__ == '__main__':
+    # exec_pipeline(os.path.join(DATA_PATH, 'USAspending_award_summaries.csv'))
+    freeze_support()
+    exec_pool()
+    end = time()
+    print(f'{end-start} secs')
     
--- gov_contracts_pipeline/manage.py
+++ gov_contracts_pipeline/manage.py
@@ -1,17 +1,27 @@
+# 
 
+from multiprocessing import freeze_support
 import settings
 import build_db
 import gov_contract_data_cleaner as gcdc
-from pandas import read_csv
+from pandas import DataFrame
 from time import time
 
 
-
-class Pipeline:
+class Pipeline ():
+    """## Task Pipeline (linear)"""
     def __init__(self):
         self.tasks = []
         
-    def task(self, depends_on=None):
+    def task(self, depends_on: object=None) -> object:
+        """## Adds tasks to the task list
+
+        Args:
+            depends_on (object, optional): Dependencies of the current process. Defaults to None.
+
+        Returns:
+            object: Inner function.
+        """        
         idx = 0
         if depends_on:
             idx = self.tasks.index(depends_on) + 1
@@ -19,42 +29,52 @@ class Pipeline:
             self.tasks.insert(idx, f)
             return f
         return inner
-    def run(self, x):
+
+    def run(self, x: DataFrame) -> DataFrame:
+        """## Runs pipeline tasks
+
+        Args:
+            x (DataFrame): The dataframe arg passed between tasks
+
+        Returns:
+            DataFrame: Processed dataframe
+        """        
         for i in self.tasks:
+            start = time()
             x = i(x)
+            end = time()
+            print(f'{str(i.__name__)}: {end-start} secs')
         return x
+
 start = time()
+# Initiates the pipeline
 pipeline = Pipeline()
 
 @pipeline.task()
-def first_task(fn=None, ask=settings.DOES_CLEAN_EXIST):
-    '''
-    Runs cleaning pipeline on data
-    args:
-        fn: [optional] (str or path-like) if None the default './data/dirty_data.csv' is used
-        returns:
-            pandas.DataFrame
-    '''
-    # check for existing file and clean data
-    if not ask:
-        data = gcdc.exec_pipeline(fn)
-        return data
-    else:
-        run_process = input('Cleaned file already exists, replace y/n? ')
-        if 'y' in run_process:
-            data = gcdc.exec_pipeline(fn)
-            return data
-        else:
-            data = read_csv(settings.DEF_CLEAN_PATH, low_memory=False)
-            return data
-        
+def first_task(fn: str=None) -> DataFrame:
+    """## Runs the multiprocessor transformer pipeline
+
+    Args:
+        fn (str, optional): Filename to clean. Defaults to None.
+
+    Returns:
+        (DataFrame): Cleaned dataframe
+    """
+    data = gcdc.exec_pool(fn)
+    return data
+
 @pipeline.task(depends_on=first_task)
-def second_task(data):
-    '''
-    Sets up database and closes connection
-    args:
-        data: Passes the data from previous function to next task
-    '''
+def second_task(data: DataFrame) -> DataFrame:
+    """## DB setup
+
+    TODO: Transition DB to postgres instead of SQLite
+
+    Args:
+        data (DataFrame): Dataframe to be passed to next task.
+
+    Returns:
+        DataFrame: Passes dataframe to next task.
+    """    
     # Check if database file exists
     need_to_create = settings.RUN_DB_SETUP
     if need_to_create:
@@ -69,28 +89,36 @@ def second_task(data):
     return data
 
 @pipeline.task(depends_on=second_task)
-def last_task(data):
-    '''
-    Inserts values into database
-    '''
-    fact_table = ['actions']
-    dim_tables = ['businesses', 'awards']
+def last_task(data: DataFrame) -> DataFrame:
+    """## Updates the DB
+
+    Args:
+        data (DataFrame): Data to be inserted/updated
+
+    Returns:
+        DataFrame: [description]
+    """    
     sql_data = {}
     db = build_db.DB(False, True)
     num_rows_to_insert = data.shape[0]
-    # insertion functionsn
-
+    
+    # Insertion loops
+    # TODO: Update below for use with postgreSQL
+    # Sorts table columns alphabetically to lineup with insertion order
     for k in db.all_tbl_cols:
         db.all_tbl_cols[k] = sorted(db.all_tbl_cols[k])
-    
+    # Extracts data to be inserted
     for table, cols in db.all_tbl_cols.items():
         sql_data[table] = tuple(list(data[cols].to_records(index=False)))
+    # Inserts data for each table
     for table, values in sql_data.items():
         val_ = ('?,'*len(values[0])).rstrip(',')
         col_names = (','.join(db.all_tbl_cols[table]).rstrip(','))
         query = f'''INSERT OR IGNORE INTO {table} ({col_names}) VALUES ({val_})'''
         db.exmany_sql(query, values)
+
     number_action_rows = db.ex_sql('SELECT COUNT(*) FROM actions')
+    # Prints DB statistics
     print('\n', '-'*30)
     print('Total DB rows modified:', db.count_changes())
     print('Rows in actions table:', number_action_rows[0][0])
@@ -98,10 +126,12 @@ def last_task(data):
     db.close_conn()
     return data
 
-
 if __name__ == '__main__':
+    # For multiprocessing Pool execution
+    freeze_support()
+    # Executes the pipeline tasks in correct order
     pipeline.run(None)
-    
+
     end = time()
     print('-'*30)
     print('seconds to complete:', round(end-start, 1))
